69a90a57c7055610204d575a64b3e4f17e209f5c
Lucas Ferreira <lucaasferreirafl@gmail.com>
2022-09-14 09:11:26 -0300

    feat: theme and fonts config

--- App.tsx
+++ App.tsx
@@ -1,5 +1,27 @@
-import { Groups } from '@screens/Groups';
+import { ActivityIndicator } from "react-native"
+
+import { ThemeProvider } from "styled-components"
+
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold
+} from "@expo-google-fonts/roboto"
+
+import { Groups } from "@screens/Groups"
+
+import theme from "@theme/index"
 
 export default function App() {
-  return <Groups />
+
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold
+  })
+
+  return (
+    <ThemeProvider theme={theme}>
+      { !fontsLoaded ? <ActivityIndicator /> : <Groups /> }
+    </ThemeProvider>
+  )
 }
