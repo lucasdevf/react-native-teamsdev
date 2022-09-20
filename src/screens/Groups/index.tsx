5ced45ebd5905bf54aa08108f55b0d317eb5ce7b
Lucas Ferreira <lucaasferreirafl@gmail.com>
2022-09-20 17:15:51 -0300

    feat: list group cards

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,20 +1,27 @@
 import { GroupCard } from "@components/GroupCard";
 import { Header } from "@components/Header";
 import { Highlight } from "@components/Highlight";
+import { useState } from "react";
+import { FlatList } from "react-native";
 import { Container } from "./styles";
 
 export function Groups() {
+
+  const [groups, setGroups] = useState(['Turma do pagode', 'Turma do padoque', 'Turma'])
+
   return (
     <Container>
       <Header />
 
       <Highlight title="Turmas" subtitle="Jogue com a sua turma" />
 
-      <GroupCard title="Turma do trabalho" />
-      <GroupCard title="Turma do padoque" />
-      <GroupCard title="Turma do pagode" />
-      <GroupCard title="Turma do job" />
-      <GroupCard title="Turma do ignite" />
+      <FlatList 
+        data={groups}
+        keyExtractor={item => item}
+        renderItem={({ item }) => (
+          <GroupCard title={item} />
+        )}
+      />
     </Container>
   )
 }
