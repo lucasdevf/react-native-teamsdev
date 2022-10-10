39fed139642545ffe058766114ac89fe75571b9b
Lucas Ferreira <lucaasferreirafl@gmail.com>
2022-10-10 19:26:54 -0300

    feat: remove glitch when move between screens

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,21 @@
 import { NavigationContainer } from '@react-navigation/native'
+import { View } from 'react-native'
+
+/* STYLES */
+import { useTheme } from 'styled-components/native'
 
 /* ROUTES */
 import { AppRoutes } from './app.routes'
 
 export function Routes() {
+
+  const { COLORS } = useTheme()
+
   return (
-    <NavigationContainer>
-      <AppRoutes />
-    </NavigationContainer>
+    <View style={{ flex: 1, backgroundColor: COLORS.GRAY_600 }}>
+      <NavigationContainer>
+        <AppRoutes />
+      </NavigationContainer>
+    </View>
   )
 }
